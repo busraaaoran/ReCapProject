47d0de337a61b09af98e2b001328fbdd0b237c15
Büşra <busraoran043@gmail.com>
2021-03-17 01:54:14 +0300

    13. gün

---
+++ SQLQuery4.sql
@@ -0,0 +1,9 @@
+﻿create table CarImages(
+
+ImageId int identity(1,1),
+CarId int,
+ImagePath nvarchar(max),
+Date datetime2(7),
+primary key (ImageId),
+foreign key (CarId) references Cars(CarId)
+);
