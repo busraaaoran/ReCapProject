906e103f65aac9930ac57f6aefe5eca3ce5d3fc3
Büşra <busraoran043@gmail.com>
2021-03-18 23:12:25 +0300

    13. gün ödevi

--- Entities/Concrete/CarImage.cs
+++ Entities/Concrete/CarImage.cs
@@ -1,12 +1,14 @@
 ﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Entities.Concrete
 {
     public class CarImage:IEntity
     {
+        [Key]
         public int ImageId { get; set; }
 
         public int CarId { get; set; }
